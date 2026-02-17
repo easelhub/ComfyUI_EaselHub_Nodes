cf4db6dfe3228dd2c844127941f5f62d7cfee4b9
EaselHub <5114559@qq.com>
2026-02-17 20:53:47 +0800

    Update ehn_model_bus.js

--- js/ehn_model_bus.js
+++ js/ehn_model_bus.js
@@ -6,35 +6,31 @@ app.registerExtension({
         if (nodeData.name === "EHN_ModelBus") {
             const onNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function() {
-                if (onNodeCreated) onNodeCreated.apply(this, arguments);
-                if (!this.inputs || this.inputs.length === 0) this.addInput("*", "*");
+                onNodeCreated?.apply(this, arguments);
+                if (!this.inputs?.length) this.addInput("*", "*");
             };
 
             const onConnectionsChange = nodeType.prototype.onConnectionsChange;
             nodeType.prototype.onConnectionsChange = function(type, index, connected, link_info, slot) {
-                if (onConnectionsChange) onConnectionsChange.apply(this, arguments);
+                onConnectionsChange?.apply(this, arguments);
                 if (type !== 1) return;
-                setTimeout(() => {
-                    if (connected && link_info) {
-                        const link = app.graph.links[link_info.link];
-                        if (link && this.inputs[index]) {
-                            this.inputs[index].name = link.type;
-                            this.inputs[index].type = link.type;
-                        }
-                    } else if (!connected && this.inputs[index]) {
-                        if (index !== this.inputs.length - 1) { 
-                            this.removeInput(index);
-                        } else {
-                            this.inputs[index].name = "*";
-                            this.inputs[index].type = "*";
-                        }
+                setTimeout(() => this.updateInputs(), 20);
+            };
+
+            nodeType.prototype.updateInputs = function() {
+                for (let i = this.inputs.length - 2; i >= 0; i--) {
+                    if (!this.inputs[i].link) this.removeInput(i);
+                }
+                for (let i = 0; i < this.inputs.length; i++) {
+                    const link = this.inputs[i].link ? app.graph.links[this.inputs[i].link] : null;
+                    const type = link ? link.type : "*";
+                    if (this.inputs[i].type !== type) {
+                        this.inputs[i].type = type;
+                        this.inputs[i].name = type;
                     }
-                    
-                    const last = this.inputs[this.inputs.length - 1];
-                    if (last && last.link !== null) this.addInput("*", "*");
-                    
-                    this.setSize(this.computeSize());
-                }, 20);
+                }
+                if (this.inputs[this.inputs.length - 1].link) this.addInput("*", "*");
+                this.setSize(this.computeSize());
             };
         }
     },
@@ -44,30 +40,27 @@ app.registerExtension({
         const originalQueuePrompt = api.queuePrompt;
         api.queuePrompt = async function(number, {output}) {
             const busSources = {};
-            for (const nodeId in output) {
-                const node = output[nodeId];
-                if (node.class_type === "EHN_ModelBus" && node.inputs) {
-                    for (const [name, value] of Object.entries(node.inputs)) {
-                        if (Array.isArray(value) && name !== "*") busSources[name] = value;
+            for (const id in output) {
+                if (output[id].class_type === "EHN_ModelBus" && output[id].inputs) {
+                    for (const [k, v] of Object.entries(output[id].inputs)) {
+                        if (Array.isArray(v) && k !== "*") busSources[k] = v;
                     }
                 }
             }
-            if (Object.keys(busSources).length > 0) {
+            if (Object.keys(busSources).length) {
                 const nodeDefs = await api.getNodeDefs();
-                for (const nodeId in output) {
-                    const node = output[nodeId];
-                    if (node.class_type === "EHN_ModelBus") continue;
-                    const def = nodeDefs[node.class_type];
-                    if (!def || !def.input) continue;
+                for (const id in output) {
+                    if (output[id].class_type === "EHN_ModelBus") continue;
+                    const def = nodeDefs[output[id].class_type];
+                    if (!def?.input) continue;
                     const inject = (inputs, config) => {
                         if (!inputs) return;
-                        for (const [inputName, inputConfig] of Object.entries(config)) {
-                            const type = inputConfig[0];
-                            if (busSources[type] && !node.inputs[inputName]) node.inputs[inputName] = busSources[type];
+                        for (const [k, v] of Object.entries(config)) {
+                            if (busSources[v[0]] && !output[id].inputs[k]) output[id].inputs[k] = busSources[v[0]];
                         }
                     };
-                    if (def.input.required) inject(node.inputs, def.input.required);
-                    if (def.input.optional) inject(node.inputs, def.input.optional);
+                    inject(output[id].inputs, def.input.required);
+                    inject(output[id].inputs, def.input.optional);
                 }
             }
             return originalQueuePrompt.apply(this, arguments);
