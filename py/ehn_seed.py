d0e7008c5f2df5ea9fc1a2c8da4c366b712c8e43
EaselHub <5114559@qq.com>
2026-02-17 19:41:14 +0800

    update

--- py/ehn_seed.py
+++ py/ehn_seed.py
@@ -1,18 +1,12 @@
-import random, time
+import random
+
 class EHN_Seed:
-    NAME, CATEGORY = "EHN_Seed", "EaselHub/Utils"
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff})}, "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "unique_id": "UNIQUE_ID"}}
-    RETURN_TYPES, RETURN_NAMES, FUNCTION = ("INT",), ("SEED",), "main"
-    @classmethod
-    def IS_CHANGED(s, seed, **kwargs):
-        return float("nan") if seed < 0 else seed
-    def main(self, seed=0, prompt=None, extra_pnginfo=None, unique_id=None):
-        if seed < 0:
-            seed = int(time.time() * 1000) % 0xffffffffffffffff
-            if unique_id and prompt and str(unique_id) in prompt: prompt[str(unique_id)]['inputs']['seed'] = seed
-            if unique_id and extra_pnginfo and 'workflow' in extra_pnginfo:
-                node = next((x for x in extra_pnginfo['workflow']['nodes'] if str(x['id']) == str(unique_id)), None)
-                if node: node['widgets_values'] = [seed]
+        return {"required": {"seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff})}}
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("seed",)
+    FUNCTION = "execute"
+    CATEGORY = "EaselHub/Utils"
+    def execute(self, seed):
         return (seed,)
